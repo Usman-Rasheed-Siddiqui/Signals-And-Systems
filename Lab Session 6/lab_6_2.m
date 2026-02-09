c21729513ab0c46d5b8745ca06e002e7c463977f
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-02-09 12:28:38 +0500

    lab 6 uploaded

---
+++ Lab Session 6/lab_6_2.m
@@ -0,0 +1,24 @@
+
+v1 = [0 0 0 0 4 4 4 4 0 0 0 0];
+v2 = [0 0 0 1 2 3 4 3 2 1 0 0];
+
+u = conv(v1, v2);
+a = 0; b = 0;
+
+p = a: a+(length(v1)-1);
+subplot(3, 1, 1), stem(p, v1); grid on;
+xlabel('Time'); ylabel('Amplitude');
+title('Input:v1(n)');
+
+q = b: b+(length(v2)-1);
+subplot(3, 1, 2), stem(q, v2); grid on;
+xlabel('Time'); ylabel('Amplitude');
+title('Input:v2(n)');
+
+n = a-length(v2)+1: length(u)+a+b-length(v2);
+subplot(3, 1, 3), stem(n, u); grid on; disp(u);
+xlabel('Time'); ylabel('Amplitude');
+title('Output: u(n)');
+
+
+
