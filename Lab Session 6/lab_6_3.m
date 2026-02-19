89f7ee685c5f8db1d078f4c594287e3bb1ace959
Usman Rasheed Siddiqui <siddiqui.uas1@gmail.com>
2026-02-19 14:20:19 +0500

    lab 5 and 6 done

--- Lab Session 6/lab_6_3.m
+++ Lab Session 6/lab_6_3.m
@@ -1,26 +1,20 @@
-
 t = -10:10;
 
 x = 1.*((t >= 0)&(t <=2));
-h = (0.5.*((t >= 0)&(t <=2)))|(1.*((t >= 4)&(t <=6)));
+h = (0.5.*((t >= 0)&(t <=2)))+(1.*((t >= 4)&(t <=6)));
 
 u = conv(x, h);
 
-a = 0; b = 0;
-
-p = a: a + (length(x) - 1);
-subplot(3, 1, 1), stem(p, x); grid on;
+subplot(3, 1, 1), stem(t, x); grid on;
 xlabel('Time'); ylabel('Amplitude');
 title('Input:x(t)');
 
-q = b: b + (length(h) - 1);
-subplot(3, 1, 2), stem(q, h); grid on;
+subplot(3, 1, 2), stem(t, h); grid on;
 xlabel('Time'); ylabel('Amplitude');
 title('Input:h(t)');
 
-n = (a + b) : (a + b + length(u) - 1);
+n = (2*t(1)):(2*t(end));
+
 subplot(3, 1, 3), stem(n, u); grid on;
 xlabel('Time'); ylabel('Amplitude');
 title('u(t):x(t)*h(t)');
-
-
